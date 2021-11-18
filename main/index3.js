da84c1f4b05003313973fe04c253cd33a3434c89
debugEverything <67364073+debugEverything@users.noreply.github.com>
2021-11-18 15:22:13 +1100

    message

---
+++ main/index3.js
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+   <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+      integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+   <script src="https://widgets.mindbodyonline.com/javascripts/healcode.js" type="text/javascript"></script>
+
+
+
+</head>
+
+<body>
+   <healcode-widget data-type="prospects" data-widget-partner="object" data-widget-id="6741774fd04"
+      data-widget-version="0">
+      <link rel="stylesheet" media="screen"
+         href="https://assets.healcode.com/assets/widgets/healcode-48cb9f1fa868bdfaf14acc40eac9a40d951eb77fe6d05ae93d5ebdfb660cca6f.css">
+
+      <style type="text/css">
+         .hc-prospect-submit {
+            font-size: 0.8rem;
+            margin-top: 10px;
+            padding: 8px;
+            color: #fff;
+            background-color: #000;
+            border-radius: 300px;
+         }
+      </style>
+      <style type="text/css">
+         .hc_footer {
+            text-align: right;
+         }
+
+         .hc-privacy-footer {
+            margin-top: 1em;
+         }
+
+         .hc_footer img,
+         .hc-privacy-footer img {
+            width: 80px;
+         }
+
+         .hc-privacy-footer .widget-privacy-link {
+            float: right;
+            margin-top: 0.6em;
+         }
+
+         .hc-privacy-footer .privacy-policy-link {
+            color: #696C74;
+         }
+      </style>
+
+      <div class="healcode prospect" id="6741774fd04" data-widget-name="prospect" data-view-type="compact"
+         data-hc-initialized="true">
+         <div id="hc-prospect-messages-6741774fd04" style="display: none;"></div>
+
+
+
+
+         <form id="prospect_form_6741774fd04" data-hc-parsley-locale="en"
+            action="https://widgets.mindbodyonline.com/widgets/prospects/6741774fd04/create?site=file%3A%2F%2F%2FUsers%2Fdaniel%2FDocuments%2Findex.html&amp;thankyou=true?pwidget=6741774fd04"
+            accept-charset="UTF-8" data-remote="true" method="post" novalidate=""><input name="utf8" type="hidden"
+               value="✓">
+            <div id="a_comment_body_hp_1637150122">
+               <style type="text/css" media="screen" scoped="scoped">
+                  #a_comment_body_hp_1637150122 {
+                     position: absolute;
+                     left: -9999px;
+                  }
+               </style><label for="a_comment_body">Do not fill in this field</label><textarea name="a_comment_body"
+                  id="a_comment_body" tabindex="-1"></textarea>
+            </div>
+            <div class="hc_prospect">
+               <div class="hc-prospect-field hc-prospect-first-name">
+                  <input placeholder="First Name" data-hc-parsley-required="true" data-hc-parsley-trigger="blur"
+                     data-hc-parsley-minlength="2" type="text" name="prospects[first_name]" id="prospects_first_name">
+               </div>
+               <div class="hc-prospect-field hc-prospect-last-name">
+                  <input placeholder="Last Name (optional)" data-hc-parsley-trigger="blur" data-hc-parsley-minlength="2"
+                     type="text" name="prospects[last_name]" id="prospects_last_name">
+               </div>
+               <div class="hc-prospect-field hc-prospect-email">
+                  <input placeholder="Email" data-hc-parsley-required="true" data-hc-parsley-trigger="blur"
+                     data-hc-parsley-type="email" type="text" name="prospects[email]" id="prospects_email">
+               </div>
+               <div class="hc-prospect-field hc-prospect-mobile-phone">
+                  <input placeholder="Phone Number (optional)" data-hc-parsley-trigger="blur"
+                     data-hc-parsley-pattern="^([+]?[0-9]{1,3}[- .])?([(]{1}[0-9]{2,6}[)])?([0-9 -.]{3,20})((x|ext|extension)[ ]?[0-9]{1,4})?$"
+                     type="text" name="prospects[mobile_phone]" id="prospects_mobile_phone">
+               </div>
+               <div>
+                  <div>Get updates on events and our latest offers</div>
+                  <span class="hc-prospect-field hc-prospect-email-opt_in">
+                     <input name="prospects[promotion_email_opt_in]" type="hidden" value="0"><input type="checkbox"
+                        value="1" checked="checked" name="prospects[promotion_email_opt_in]"
+                        id="prospects_promotion_email_opt_in"
+                        data-hc-parsley-multiple="prospectspromotion_email_opt_in">
+                     Email
+                  </span>
+                  <span class="hc-prospect-field hc-prospect-sms-opt_in">
+                     <input name="prospects[promotion_sms_opt_in]" type="hidden" value="0"><input type="checkbox"
+                        value="1" checked="checked" name="prospects[promotion_sms_opt_in]"
+                        id="prospects_promotion_sms_opt_in" data-hc-parsley-multiple="prospectspromotion_sms_opt_in">
+                     Text
+                  </span>
+               </div>
+
+               <div class="hc-actions">
+                  <input type="submit" name="commit" value="Submit" class="submit hc-prospect-submit"
+                     id="hc-prospect-6741774fd04" data-disable-with="Submit">
+                  <img id="hc-prospect-spinner-6741774fd04" style="display:none;"
+                     src="https://assets.healcode.com/assets/widgets/spinner-10fbb9196ed28f24a07be2de7f0c6f5d1704fe4d798fa7e51f9889430e787cfd.gif">
+               </div>
+
+            </div>
+         </form>
+         <div class="hc-privacy-footer">
+            <a href="https://www.mindbodyonline.com" alt="Mindbody Inc." title="Mindbody Inc." target="_blank"
+               rel="nofollow noopener noreferrer"><img
+                  src="https://assets.healcode.com/assets/widgets/mb-powered-logo-vertical-b3baead5e5bf4235998170260d79972f8d54ad32ac5136c61baff85f09fc539c.svg"></a>
+            <span class="widget-privacy-link"><a class="privacy-policy-link" target="_blank"
+                  rel="nofollow noopener noreferrer" title="Mindbody Inc."
+                  href="https://www.mindbodyonline.com/privacy-policy">Privacy Policy</a></span>
+         </div>
+
+      </div>
+
+
+   </healcode-widget>
+</body>
+
+</html>
